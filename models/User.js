4e0de290df8f03b7fdbdf2545b01e539eb53abfa
KarenRoth <karenrothc@gmail.com>
2020-11-16 13:33:08 -0300

    test

--- models/User.js
+++ models/User.js
@@ -2,8 +2,25 @@ const mongoose = require('mongoose');
 const Schema   = mongoose.Schema;
 
 const userSchema = new Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+},
+  carritoDeCompras: {
+    
+  }
   username: String,
-  password: String
+  password: String,
+  role: {
+    type: String,
+    enum: ['ADMIN', 'VENDEDOR', 'COMPRADOR'],
+    default: 'USUARIO'
+},
+    facebookID: String,
+    googleID: String,
+    microsoftID: String
+
 }, {
   timestamps: {
     createdAt: 'created_at',
