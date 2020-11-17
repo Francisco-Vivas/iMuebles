5f79c12aed172f834cea956c7affd1fa60f89c02
Francisco Vivas <franciscovivas.codes@gmail.com>
2020-11-17 12:16:17 -0500

    Add products categories, update views and fix edit product bug

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,13 +1,17 @@
-const productController = require('../controllers/product.controller');
-const fileUploader = require('../configs/cloudinary.configs');
+const productController = require("../controllers/product.controller");
+const fileUploader = require("../configs/cloudinary.configs");
 
-const router = require('express').Router();
-  
-router.get('/', productController.list);
-router.get('/new', productController.showFormNew);
-router.post('/new', fileUploader.single('imagesURL'), productController.create);
-router.get('/:productId', productController.showDetails);
-router.get('/:productId/edit', productController.editProductView);
-router.post('/:productId/edit', fileUploader.single('imagesURL'), productController.editProduct);
+const router = require("express").Router();
+
+router.get("/", productController.list);
+router.get("/new", productController.showFormNew);
+router.post("/new", fileUploader.single("imagesURL"), productController.create);
+router.get("/:productId", productController.showDetails);
+router.get("/:productId/edit", productController.editProductView);
+router.post(
+  "/:productId/edit",
+  fileUploader.single("imagesURL"),
+  productController.editProduct
+);
 
 module.exports = router;
