97948a5b5616f3185323edf717f299f9263eef6d
Francisco Vivas <franciscovivas.codes@gmail.com>
2020-11-18 13:52:26 -0500

    Fix cart bugs, Add new user cart per purchase & Add prices format

--- passport/facebookStragety.js
+++ passport/facebookStragety.js
@@ -1,27 +1,31 @@
-const FacebookStrategy = require('passport-facebook');
+const FacebookStrategy = require("passport-facebook");
 const passport = require("passport");
 const User = require("../models/User");
+const { createNewCart } = require("../controllers/cart.controller");
 
 passport.use(
   new FacebookStrategy(
     {
       clientID: process.env.FACEBOOK_ID,
       clientSecret: process.env.FACEBOOK_SECRET,
-      callbackURL: '/auth/facebook/callback',
-      profileFields: ['id', 'email', 'name'],
+      callbackURL: "/auth/facebook/callback",
+      profileFields: ["id", "email", "name"],
     },
-    async (_,__ , profile, callback) => {
-      const user = await User.findOne({ facebookId: profile.id })
+    async (_, __, profile, callback) => {
+      const user = await User.findOne({ facebookId: profile.id });
       if (!user) {
+        const cartId = await createNewCart();
+
         const userCreated = await User.create({
           name: `${profile.name.givenName} ${profile.name.familyName}`,
           facebookId: profile.id,
           email: profile.emails[0].value,
-        })
-        return callback(null, userCreated)
+          cartId,
+        });
+        return callback(null, userCreated);
       } else {
-        callback(null, user)
+        callback(null, user);
       }
     }
   )
-)
+);
