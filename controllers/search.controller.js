6a8b9d8f255798f501f6efc072c1482845741baa
Francisco Vivas <franciscovivas.codes@gmail.com>
2020-11-18 20:22:39 -0500

    Add search bar with regex

--- controllers/search.controller.js
+++ controllers/search.controller.js
@@ -1,7 +1,28 @@
-exports.searchBar = (req, res) => {
-  console.log("searchBar ===============================");
+const ProductModel = require("../models/Product.model");
+
+exports.searchBar = async (req, res) => {
+  console.log(
+    "============================ searchBar ========================================="
+  );
   const { product } = req.body;
-  const searchWords = product.split(" ");
-  console.log(searchWords, product);
+  const searchWords = product.split(" ").join("|");
+
+  const products = await ProductModel.find({
+    $or: [
+      {
+        name: {
+          $regex: searchWords,
+          $options: "i",
+        },
+      },
+      {
+        category: {
+          $regex: searchWords,
+          $options: "i",
+        },
+      },
+    ],
+  });
+  console.log("products", products);
   res.redirect("/products");
 };
