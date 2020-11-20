130de49bcc2de13c19d726b7261566eb1a52dbc7
Francisco Vivas <franciscovivas.codes@gmail.com>
2020-11-20 11:13:08 -0500

    Improve style of new/edit forms and use textarea insted of an input-text

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,15 @@ const productModel = require("../models/Product.model");
 
 /* GET home page */
 router.get("/", async (req, res, next) => {
-  let categoryViewSillas = await productModel.find({ category: ["Sillas"] });
-  let categoryViewEsc = await productModel.find({ category: ["Escritorios"] });
-  let categoryViewSof = await productModel.find({ category: ["Sofás"] });
+  let categoryViewSillas = await productModel
+    .find({ category: ["Sillas"] })
+    .limit(5);
+  let categoryViewEsc = await productModel
+    .find({ category: ["Escritorios"] })
+    .limit(5);
+  let categoryViewSof = await productModel
+    .find({ category: ["Sofás"] })
+    .limit(5);
   res.render("index", { categoryViewSillas, categoryViewEsc, categoryViewSof });
 });
 
