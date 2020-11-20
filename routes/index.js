37c3faae6cce2f8de2c075957b739ad6fa7eefbc
KarenRoth <karenrothc@gmail.com>
2020-11-19 23:46:17 -0300

    avances home

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router  = express.Router();
 const { sendMensajito } = require('../controllers/nodemailerController');
+const productModel = require('../models/Product.model')
 
 /* GET home page */
-router.get('/', (req, res, next) => {
-  console.log(req.user)
-  res.render('index');
+router.get('/', async (req, res, next) => {
+  const categoryView = await productModel.find({category:["Sillas", "Escritorios", "Sofás"], imageURL:[0]});
+  // console.log(req.user)
+  console.log (categoryView)
+  res.render('index', {categoryView});
 });
+
 
 
 router.post('/send-email', sendMensajito);
